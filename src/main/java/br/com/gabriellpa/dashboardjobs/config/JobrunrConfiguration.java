96d326b0cc3b5d40739481436d6a404edacefc9f
Gabriel Pires Antunes <gabriellpantunes@gmail.com>
2021-12-05 15:04:37 -0300

    chore: configuração para o docker

--- src/main/java/br/com/gabriellpa/dashboardjobs/config/JobrunrConfiguration.java
+++ src/main/java/br/com/gabriellpa/dashboardjobs/config/JobrunrConfiguration.java
@@ -6,15 +6,16 @@ import org.jobrunr.storage.StorageProvider;
 import org.jobrunr.storage.nosql.redis.JedisRedisStorageProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
 
 
 @Configuration
 public class JobrunrConfiguration {
 
-        @Bean
-        public StorageProvider storageProvider(JobMapper jobMapper) {
-            JedisRedisStorageProvider jedisRedisStorageProvider = new JedisRedisStorageProvider();
-            jedisRedisStorageProvider.setJobMapper(jobMapper);
-            return jedisRedisStorageProvider;
-        }
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper) {
+        JedisRedisStorageProvider jedisRedisStorageProvider = new JedisRedisStorageProvider(new JedisPool("redis", 6379));
+        jedisRedisStorageProvider.setJobMapper(jobMapper);
+        return jedisRedisStorageProvider;
+    }
 }
